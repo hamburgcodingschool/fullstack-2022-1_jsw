02780b83388e4eafd15cc4eeaecd02aae7b67bb0
Pedro Remoaldo <premoaldo@gmail.com>
2022-03-11 17:40:16 +0000

    Tasks v2

--- code/dom4tasks.js
+++ code/dom4tasks.js
@@ -3,19 +3,35 @@ let taskField=document.querySelector("#task");
 let ulTasks=document.querySelector("#listOfTasks");
 
 function insertTask() {
-    let task=taskField.value;
+    let task=taskField.value.trim();
     console.log(task);
     if (task==="") {
         alert("You must provide a task!");
+        taskField.focus();
     } else {
         // <li>task</li>
         // let listItem=document.createElement("li"); /* <li></li> */
-        // let content=document.createTextNode(task);
+        // let content=document.createTextNode(task+" ");
         // listItem.appendChild(content); /* <li>sleep</li> */
+        // let button = document.createElement("button");
+        // buttonInsert.setAttribute("type","button");
+        // let buttonContent=document.createTextNode("X");
+        // button.appendChild(buttonContent);
+        // button.addEventListener("click", function() {
+        //  this.parentElement.remove();
+        // });
+        // listItem.appendChild(button);
         // ulTasks.appendChild(listItem);
 
         // ulTasks.innerHTML+="<li>"+task+"</li>";
-        ulTasks.innerHTML+=`<li>${task}</li>`;
+        ulTasks.innerHTML+=`<li>${task} <button type="button">X</button></li>`;
+        let buttons=document.querySelectorAll("#listOfTasks button");
+        for (let button of buttons) {
+            button.addEventListener("click", function() {
+                // console.log("Yes");
+                this.parentElement.remove();
+            });
+        }
         taskField.value="";
         taskField.focus();
     }
